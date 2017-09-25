6b0363d6615d7a129eff66fe613e8323c4719458
Frank Sciallo <fsciallo@dtcc.edu>
2017-09-25 15:46:22 -0400

    Add files via upload

---
+++ test.sql
@@ -0,0 +1,10 @@
+CREATE DATABASE test;
+USE test;
+CREATE TABLE persons( name VARCHAR(25), email VARCHAR(25) );
+INSERT INTO persons VALUES('Ben R.', 'ben@cirrus.com');
+INSERT INTO persons VALUES('Eric H.','eric@altostratus.com');
+INSERT INTO persons VALUES('Colin T.','colin@nibostratus.com');
+INSERT INTO persons VALUES('Alex B.','alex@stratocumulus.com');
+INSERT INTO persons VALUES('James G.','james@cumulonimbus.com');
+
+
